00a5a518a04e2082047633a9c7493d3674360835
saipuneet <saipuneethpogula7@gmail.com>
2022-11-02 18:48:29 -0500

    updated

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var appRouter = require('./routes/Cap');
+var gridbulidRouter = require('./routes/gridbulid');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Cap', appRouter);
+app.use('/gridbulid', gridbulidRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
